0bda5bc8c6291ee5ce0580aa95baeb7f709465e8
unknown <baasandres.56@hotmail.com>
2022-10-13 19:58:24 -0500

    updating

--- Udemy/Leccion01/booleanos.py
+++ Udemy/Leccion01/booleanos.py
@@ -3,12 +3,13 @@
 miVariable = False
 print(miVariable)
 
+
 # Pregunta que regresa verdadero o falso
 miVariable = 3 > 2
 print(miVariable)
 
-# este if y este else nos permite ver que resultado tuvo la variable "miVariable"
 
+# este if y este else nos permite ver que resultado tuvo la variable "miVariable"
 if miVariable:
     print("El resultado fue verdadero")
 else:
